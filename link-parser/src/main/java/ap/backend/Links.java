b34dc624f845257eb13d57a8d2b6bc4397124c68
KireevRuslan <124637349+KireevRuslan@users.noreply.github.com>
2023-03-21 19:42:13 +0500

    try 1

---
+++ link-parser/src/main/java/ap/backend/Links.java
@@ -0,0 +1,7 @@
+package ap.backend;
+
+public sealed interface Links permits GitHubURL, StackOverflowURL {
+    public default String getData(String URL){
+        return "null";
+    }
+}
---
+++ link-parser/src/main/java/ap/backend/StackOverflowURL.java
@@ -0,0 +1,7 @@
+package ap.backend;
+
+public final class StackOverflowURL implements Links{
+    public String getData(String URL){
+        return URL.split("/")[4];
+    }
+}
