939b44a4186edeeac56a25fed38b92e7ec79d51c
KireevRuslan <124637349+KireevRuslan@users.noreply.github.com>
2023-04-11 23:26:46 +0500

    изменил функционал парсера

--- link-parser/src/main/java/ap/backend/App.java
+++ link-parser/src/main/java/ap/backend/App.java
@@ -10,12 +10,5 @@ import java.util.Scanner;
 
 public class App
 {
-    public static void main( String[] args ) throws URISyntaxException {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Введите ссылку: ");
-        String URL = sc.nextLine();
-        BackendLinkParser linkParser = new BackendLinkParser(URL);
-        System.out.println(linkParser.getData().toString());
-    }
 }
