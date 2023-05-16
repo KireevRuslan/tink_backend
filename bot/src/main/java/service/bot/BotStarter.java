ad72be1221afe64397b13019713eecbd61260c98
KireevRuslan <124637349+KireevRuslan@users.noreply.github.com>
2023-05-17 01:24:29 +0500

    докер логи

--- bot/src/main/java/service/bot/BotStarter.java
+++ bot/src/main/java/service/bot/BotStarter.java
@@ -6,31 +6,32 @@ import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.request.SetMyCommands;
+import io.micrometer.core.instrument.Counter;
+import java.util.List;
 import org.springframework.stereotype.Service;
 import configuration.TelegramConfig;
-import service.bot.Bot;
-import service.commands.CommandList;
 import service.commands.Command;
+import service.commands.CommandList;
 import service.commands.impl.UnknownCommand;
-
-import java.util.List;
 
 @Service
 public class BotStarter implements Bot {
     private final TelegramBot bot;
     private final CommandList commandList;
+    private final Counter counter;
 
-    public BotStarter(TelegramConfig telegramConfig, CommandList commandList) {
+    public BotStarter(TelegramConfig telegramConfig, CommandList commandList, Counter counter) {
         this.bot = new TelegramBot("5810088925:AAHRFFT26JpExA27t927RMNh7zGdYwtImk0");
         this.commandList = commandList;
+        this.counter = counter;
         botCommands();
     }
 
     private void botCommands() {
         List<Command> commands = commandList.getCommandList();
         BotCommand[] botCommands = commands.stream()
-                .map(Command::toApiCommand)
-                .toArray(BotCommand[]::new);
+            .map(Command::toApiCommand)
+            .toArray(BotCommand[]::new);
         SetMyCommands request = new SetMyCommands(botCommands);
         bot.execute(request);
     }
@@ -40,6 +41,7 @@ public class BotStarter implements Bot {
             Message message = update.message();
             if (message != null) {
                 SendMessage sendMessage = handleByCommand(update, message);
+                counter.increment();
                 bot.execute(sendMessage);
             }
         });
@@ -48,11 +50,11 @@ public class BotStarter implements Bot {
 
     public SendMessage handleByCommand(Update update, Message message) {
         Command command = commandList.getCommandList()
-                .stream()
-                .filter(it -> it.command()
-                        .equals(message.text()))
-                .findFirst()
-                .orElseGet(UnknownCommand::new);
+            .stream()
+            .filter(it -> it.command()
+                .equals(message.text()))
+            .findFirst()
+            .orElseGet(UnknownCommand::new);
         return command.handle(update);
     }
 
